4c2fed398d42203f3ec872c2132a4ac3c016fab9
adrianjmatthews <adrian.j.matthews@gmail.com>
2016-09-23 20:48:19 +0100

    Create mean.py

---
+++ mean.py
@@ -0,0 +1,26 @@
+import data_analysis as da
+import iris
+import iris.quickplot as qplt
+import matplotlib.pyplot as plt
+
+VAR_NAME='vwnd'; LEVEL=1000; SOURCE='ncepdoe_plev_d'; TDOMAINID='djf8283_8384'
+
+VERBOSE=2
+
+descriptor={}
+descriptor['var_name']=VAR_NAME
+descriptor['level']=LEVEL
+descriptor['source']=SOURCE
+descriptor['tdomainid']=TDOMAINID
+descriptor['basedir']='/gpfs/afm/matthews/data/'
+descriptor['filein1']=descriptor['basedir']+descriptor['source']+'/raw_std/'+descriptor['var_name']+'_'+str(descriptor['level'])+'_*.nc'
+descriptor['fileout1']=descriptor['basedir']+descriptor['source']+'/processed/'+descriptor['var_name']+'_'+str(descriptor['level'])+'_'+descriptor['tdomainid']+'.nc'
+
+aa=da.TimeDomStats(descriptor,verbose=VERBOSE)
+
+aa.event_means()
+aa.f_time_mean()
+
+#print('# Plot')
+#qplt.contourf(aa.cube_event_means)
+#plt.show()
