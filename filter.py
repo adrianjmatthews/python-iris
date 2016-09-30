5b43ad9642f5bbe5926d2e96f5a83e71838e9154
adrianjmatthews <adrian.j.matthews@gmail.com>
2016-09-30 14:42:33 +0100

    Added quick check plot to filter.py

--- filter.py
+++ filter.py
@@ -12,6 +12,10 @@ FILE_WEIGHTS='/gpfs/home/e058/home/data/weights/w_'+FILTER+'.txt'
 
 VERBOSE=2
 
+PLOT=True
+
+#=====================================================================
+
 descriptor={}
 descriptor['file_weights']=FILE_WEIGHTS
 descriptor['var_name']=VAR_NAME
@@ -36,3 +40,20 @@ for year in [1983,]:
     aa=da.TimeFilter(descriptor,verbose=VERBOSE)
     aa.time_filter()
 
+if PLOT:
+    print('# Plot')
+    time_constraint=iris.Constraint(time = lambda cell: time1 <= cell <= time2)
+    tol=0.1
+    lon0=0.0
+    lon_constraint=iris.Constraint(longitude = lambda cell: lon0-tol <= cell <= lon0+tol)
+    lat0=55.0
+    lat_constraint=iris.Constraint(latitude = lambda cell: lat0-tol <= cell <= lat0+tol)
+    with iris.FUTURE.context(cell_datetime_objects=True):
+        x1=aa.data_in.extract(time_constraint & lon_constraint & lat_constraint)
+    x2=aa.data_out.extract(lon_constraint & lat_constraint)
+    qplt.plot(x1,label='in')
+    qplt.plot(x2,label='out')
+    plt.legend()
+    plt.axis('tight')
+    qplt.show()
+    
